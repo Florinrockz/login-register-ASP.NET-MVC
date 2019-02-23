de59291fbced7b1b96c888900b5ce24d339fa0d0
Scutaru Florin - Adrian <33432925+Florinrockz@users.noreply.github.com>
2019-02-23 17:13:39 +0200

    first commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoginRegisterMVC.Models;
+
+namespace LoginRegisterMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            using (OurDbcontext dc = new OurDbcontext())
+            {
+                return View(dc.userAccount.ToList());
+            }
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserAccount account)
+        {
+            if (ModelState.IsValid)
+            {
+                using (OurDbcontext dc = new OurDbcontext())
+                {
+                    dc.userAccount.Add(account);
+                    dc.SaveChanges();
+                }
+
+                ModelState.Clear();
+
+                ViewBag.Message = account.FirstName + " " + account.LastName + " successfully registered!";
+            }
+
+            return View();
+
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserAccount us)
+        {
+            using(OurDbcontext dc = new OurDbcontext())
+            {
+                var user = dc.userAccount.Single(ua => ua.Username == us.Username && ua.Password == us.Password);
+
+                if (user!=null)
+                {
+                    Session["UserId"] = user.Id.ToString();
+                    Session["Username"] = user.Username.ToString();
+                    return RedirectToAction("LoggedIn");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Username or Password is wrong");
+                }
+            }
+
+            return View();
+        }
+
+        public ActionResult LoggedIn()
+        {
+            if (Session["UserId"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+    }
+}
---
+++ Models/UserAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace LoginRegisterMVC.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "First Name Required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name Required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email Required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Username Required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password Required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password",ErrorMessage = "Confirm your password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
